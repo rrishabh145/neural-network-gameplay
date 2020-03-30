2946f16da12fa5e0d0270dd39425358c5ac924ee
Rishabh <rrishabh145@gmail.com>
2020-03-30 16:44:22 +0530

    Function to initialize tf NN model added

--- open_AI_game.py
+++ open_AI_game.py
@@ -7,13 +7,15 @@ from tflearn.layers.estimator import regression
 from statistics import mean, median
 from collections import Counter
 
-LR = 1e-3 #learning rate
+LR = 1e-3 #learning rate for NN
 env = gym.make('CartPole-v0') #selecting a game environment
 env.reset()
-goal_steps = 500
-score_requirement = 50
-initial_games = 10000
 
+goal_steps = 500
+score_requirement = 50 #minimum score for a random model to acieve to be included in training data
+initial_games = 10000 #number of games worth of data to collect for training
+
+# function to run a game with random values
 def random_game_first():
     for episode in range(5): # number of games
         env.reset()
@@ -24,6 +26,7 @@ def random_game_first():
             if done:
                 break
 
+# function to create initial training data
 def initial_population():
     training_data = [] #observation and moves made
     scores = []
@@ -64,10 +67,41 @@ def initial_population():
     training_data_save = np.array(training_data)
     np.save('saved.npy', training_data_save) #saving the trainign data in npy arrays
 
-    print('Average accepted score:', mean(accepted_scores))
+    print('Average accepted score:', mean(accepted_scores)) #checking the average score of training samples
     print('Median accepted score:', median(accepted_scores))
     print(Counter(accepted_scores)) # seeing amount of data we obtained
 
     return training_data
 
-initial_population()
+# function to define a tf model
+def neural_network_model(input_size):
+    network = input_data(shape = [None, input_size, 1], name = 'input' ) #initializing the size of input data
+
+    # creating a layered network of NN
+
+    network = fully_connected(network, 128, activation = 'relu')
+    network = dropout(network, 0.8) # 0.2 is dropout
+
+    network = fully_connected(network, 256, activation = 'relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 512, activation = 'relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation = 'relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 128, activation = 'relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 2, activation = 'softmax') #output is one hot encoded binary
+    # initializing optimization variables for the NN
+    network = regression(network, optimizer='adam', learning_rate = LR, loss = 'categorical_crossentropy', name = 'targets')
+
+    model = tflearn.DNN(network, tensorboard_dir = 'log')
+
+    return model
+
+
+
+#initial_population()
