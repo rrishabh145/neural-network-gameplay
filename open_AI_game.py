1a0cf705528b13a605d9f6c21fa809a0b403a2ac
Rishabh <rrishabh145@gmail.com>
2020-03-30 12:48:41 +0530

    Added function to run random game

--- open_AI_game.py
+++ open_AI_game.py
@@ -1,11 +1,25 @@
 import gym
-env = gym.make("CartPole-v1")
-observation = env.reset()
-for _ in range(5000):
-    env.render()
-    action = env.action_space.sample() # your agent here (this takes random actions)
-    observation, reward, done, info = env.step(action)
+import random
+import numpy as np 
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import mean, median
+from collection import Counter
 
-    if done:
-        observation = env.reset()
-env.close()
+LR = 1e-3 #learning rate
+env = gym.make('CartPole-v0') #selecting a game environment
+env.reset()
+goal_steps = 500
+score_requirement = 50
+initial_games = 10000
+
+def random_game_first():
+    for episode in range(5): # number of games
+        env.reset()
+        for t in range(goal_steps):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
